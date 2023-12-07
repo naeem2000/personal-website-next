b10099b3843048f128d239640dc52d29b7ef08f6
Naeem Carr <naeem@liquidthought.co.za>
2023-12-07 14:43:08 +0200

    started portfolio section

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+export default function Portfolio() {
+	return (
+		<section className='portfolio'>
+			<div>
+				<span data-aos='fade-left'>{`(!)`}</span>
+				<h1 data-aos='fade-right'>{'<portfolio>'}</h1>
+				<p data-aos='fade-up'>
+					Elevating Standards, Defining Excellence: My Projects Portfolio
+				</p>
+			</div>
+		</section>
+	);
+}
