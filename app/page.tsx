4574ba4b27b8edeed6923b40b0dab68a738ee69c
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2023-12-21 11:35:04 +0200

    added custom cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ProgressBar } from '@nadfri/react-scroll-progress-bar';
+import AnimatedCursor from 'react-animated-cursor';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ScrollToTop from 'react-scroll-to-top';
@@ -14,18 +15,38 @@ export default function Home() {
 	const { Nav, Line, Hero, About, Portfolio, Services, Contact } = Routes();
 
 	useEffect(() => {
+		document.getElementById('loader')?.classList.add('slide');
 		AOS.init();
-		setTimeout(() => {
-			document.getElementById('loader')?.classList.add('slide');
-		}, 500);
+		setTimeout(() => {}, 500);
 	});
 
 	return (
 		<>
-			<ToastContainer />
+			<AnimatedCursor
+				innerSize={17}
+				outerSize={7}
+				color='231, 219, 116'
+				innerScale={0.7}
+				outerScale={5}
+				outerStyle={{ background: 'rgb(46, 204, 239, 0.4)' }}
+				clickables={[
+					'a',
+					'input[type="text"]',
+					'input[type="email"]',
+					'input[type="number"]',
+					'input[type="submit"]',
+					'input[type="image"]',
+					'label[for]',
+					'select',
+					'textarea',
+					'button',
+					'.link',
+				]}
+			/>
 			<div className='progress-bar'>
 				<ProgressBar position='fixed' color1='#2fcbef' color2='#E6DB74' />
 			</div>
+			<ToastContainer />
 			<Loader />
 			<Line />
 			<Nav />
