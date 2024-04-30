7b3a0327a16c7e0ea8f99e59a1941789b66707ec
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-04-30 11:45:40 +0200

    almost done slider

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,45 +1,32 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import { pageData } from '../components/data';
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 import Slider from 'react-slick';
-
 import Image from 'next/image';
-import React from 'react';
 
 export default function Portfolio() {
+	const [mobile, setMobile] = useState<boolean>(false);
+
 	const settings = {
 		dots: true,
-		infinite: false,
 		speed: 500,
-		slidesToShow: 3,
+		slidesToShow: mobile ? 1 : 3,
+		infinite: false,
 		slidesToScroll: 1,
 		className: 'slider',
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 2,
-				},
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-				},
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-				},
-			},
-		],
 	};
+
+	useEffect(() => {
+		if (window?.innerWidth < 720) {
+			setMobile(true);
+		} else {
+			setMobile(false);
+		}
+	}, []);
+
 	return (
 		<section className='portfolio' id='portfolio'>
 			<div className='portfolio-head'>
@@ -50,48 +37,46 @@ export default function Portfolio() {
 				</p>
 			</div>
 			<div className='portfolio-body'>
-				<div className='portfolio-items'>
-					<Slider {...settings}>
-						{pageData.mobile.map((item) => {
-							return (
-								<div className='project' key={item.id}>
-									<a href={item.link} target='_blank' rel='noopener noreferrer'>
-										<Image
-											className='img'
-											src={item.image}
-											width={350}
-											height={350}
-											alt={item.alt}
-										/>
-									</a>
-									<h3>{item.title}</h3>
-									<p>{item.description}</p>
-								</div>
-							);
-						})}
-					</Slider>
-				</div>
-				<div className='portfolio-items'>
-					<Slider {...settings}>
-						{pageData.web.map((item) => {
-							return (
-								<div className='project' key={item.id}>
-									<a href={item.link} target='_blank' rel='noopener noreferrer'>
-										<Image
-											className='img'
-											src={item.image}
-											width={350}
-											height={350}
-											alt={item.alt}
-										/>
-									</a>
-									<h3>{item.title}</h3>
-									<p>{item.description}</p>
-								</div>
-							);
-						})}
-					</Slider>
-				</div>
+				<h3>Web Apps</h3>
+				<Slider {...settings}>
+					{pageData.web.map((item) => {
+						return (
+							<div className='project' key={item.id}>
+								<a href={item.link} target='_blank' rel='noopener noreferrer'>
+									<Image
+										className='img'
+										src={item.image}
+										width={350}
+										height={350}
+										alt={item.alt}
+									/>
+								</a>
+								<h3>{item.title}</h3>
+								<p>{item.description}</p>
+							</div>
+						);
+					})}
+				</Slider>
+				<h3>Mobile Apps</h3>
+				<Slider {...settings}>
+					{pageData.mobile.map((item) => {
+						return (
+							<div className='project' key={item.id}>
+								<a href={item.link} target='_blank' rel='noopener noreferrer'>
+									<Image
+										className='img'
+										src={item.image}
+										width={300}
+										height={200}
+										alt={item.alt}
+									/>
+								</a>
+								<h3>{item.title}</h3>
+								<p>{item.description}</p>
+							</div>
+						);
+					})}
+				</Slider>
 			</div>
 		</section>
 	);
