c4e7664b03cdbaa4d6a5fc5865a023e2c5223cc7
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2025-01-02 18:14:26 +0200

    fix(portfolio): worked on portfolio page

--- app/portfolio/Portfolio.tsx
+++ app/portfolio/Portfolio.tsx
@@ -1,5 +1,31 @@
+import PageHeader from '../components/global/PageHeader';
+import Contact from '../components/Contact';
 import React from 'react';
 
 export default function Portfolio({}) {
-	return <div>Portfolio</div>;
+	const buttonStyles = 'flex item-center justify-center mr-2 text-2xl';
+	return (
+		<section>
+			<div className='wrapper'>
+				<div className='flex items-center justify-center flex-col'>
+					<PageHeader text='"Portfolio"' symbol='{!}' />
+					<p className='text-center text-yellow text-2xl mt-5 max-w-5xl'>
+						…Welcome to My Portfolio: A Deep Dive Into My Creative Journey,
+						Professional Expertise, and Passion-Driven Projects. Here, I invite
+						you to explore a collection of my best work, where creativity meets
+						precision, and innovation drives impactful results.
+					</p>
+				</div>
+			</div>
+			<div className='mt-14'>
+				<div className='flex items-center justify-center'>
+					<button className={buttonStyles}>awe</button>
+					<button className={buttonStyles}>awe</button>
+					<button className={buttonStyles}>awe</button>
+					<button className={buttonStyles}>awe</button>
+				</div>
+			</div>
+			<Contact />
+		</section>
+	);
 }
