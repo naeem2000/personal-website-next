02ecedaee1f574337ce066236bd9df862250fc57
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2025-05-13 20:52:23 +0200

    fix(layout): fixed layout issues

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -26,89 +26,96 @@ export default function ContactForm() {
 
 	if (state.succeeded) localStorage.setItem('sent', 'emailSent');
 
-	if (state.succeeded || sentEmail === true) return <ThankYou />;
-
 	return (
-		<div className='wrapper'>
-			<motion.section {...fadeInMotionStyles}>
-				<div className='text-center'>
-					{path === '/contact' ? (
-						<PageHeader text='Contact' symbol='{*}' />
-					) : (
-						<SectionHeader
-							symbol={`(*)`}
-							aos=''
-							link='/contact'
-							heading={'<contact_me/>'}
-						/>
-					)}
-					<p className='text-yellow text-xl desktop:text-[23px] leading-6 mt-5 dark:text-black'>
-						Get in Touch: Let&apos;s Collaborate and Innovate Together
-					</p>
-				</div>
-				<form
-					onSubmit={submitForm}
-					className='flex items-center justify-center flex-col'
-				>
-					<input
-						className={`${ContactStyles} ${
-							isErrors.name && 'border-b-red-600'
-						}`}
-						name='name'
-						type='text'
-						placeholder={`${isErrors.name ? 'Can I get your name?' : '_name'}`}
-						value={messenger.name}
-						onChange={(e) =>
-							setMessenger({ ...messenger, name: e.target.value })
-						}
-					/>
-					<input
-						className={`${ContactStyles} ${
-							isErrors.email && 'border-b-red-600'
-						}`}
-						name='email'
-						type='email'
-						placeholder={`${
-							isErrors.email == true
-								? 'I would like say hello back :)'
-								: '_email'
-						}`}
-						value={messenger.email}
-						onChange={(e) =>
-							setMessenger({ ...messenger, email: e.target.value })
-						}
-					/>
-					<textarea
-						className={`${ContactStyles} ${
-							isErrors.message && 'border-b-red-600'
-						}`}
-						name='message'
-						cols={10}
-						rows={7}
-						placeholder={`${
-							isErrors.message
-								? 'I would like to say thank you for reaching out to me :D'
-								: '_message'
-						}`}
-						value={messenger.message}
-						onChange={(e) =>
-							setMessenger({ ...messenger, message: e.target.value })
-						}
-					></textarea>
-					<div className='submit-area'>
-						<Button
-							className='button text-xl mt-10 bg-blue text-black transition-all duration-[0.5s] ease-[ease] px-[30px] py-3'
-							type='submit'
-						>
-							{state.submitting ? (
-								<BeatLoader style={{ margin: 0 }} size={10} />
-							) : (
-								`{submit}`
-							)}
-						</Button>
+		<motion.section
+			{...fadeInMotionStyles}
+			className={path !== '/contact' ? 'wrapper' : ''}
+		>
+			{state.succeeded || !sentEmail ? (
+				<ThankYou />
+			) : (
+				<motion.section {...fadeInMotionStyles}>
+					<div className='text-center'>
+						{path === '/contact' ? (
+							<PageHeader text='Contact' symbol='{*}' />
+						) : (
+							<SectionHeader
+								symbol={`(*)`}
+								aos=''
+								link='/contact'
+								heading={'<contact_me/>'}
+							/>
+						)}
+						<p className='text-yellow text-xl desktop:text-[23px] leading-6 mt-5 dark:text-black'>
+							Get in Touch: Let&apos;s Collaborate and Innovate Together
+						</p>
 					</div>
-				</form>
-			</motion.section>
-		</div>
+					<form
+						onSubmit={submitForm}
+						className='flex items-center justify-center flex-col'
+					>
+						<input
+							className={`${ContactStyles} ${
+								isErrors.name && 'border-b-red-600'
+							}`}
+							name='name'
+							type='text'
+							placeholder={`${
+								isErrors.name ? 'Can I get your name?' : '_name'
+							}`}
+							value={messenger.name}
+							onChange={(e) =>
+								setMessenger({ ...messenger, name: e.target.value })
+							}
+						/>
+						<input
+							className={`${ContactStyles} ${
+								isErrors.email && 'border-b-red-600'
+							}`}
+							name='email'
+							type='email'
+							placeholder={`${
+								isErrors.email == true
+									? 'I would like say hello back :)'
+									: '_email'
+							}`}
+							value={messenger.email}
+							onChange={(e) =>
+								setMessenger({ ...messenger, email: e.target.value })
+							}
+						/>
+						<textarea
+							className={`${ContactStyles} ${
+								isErrors.message && 'border-b-red-600'
+							}`}
+							name='message'
+							cols={10}
+							rows={7}
+							placeholder={`${
+								isErrors.message
+									? 'I would like to say thank you for reaching out to me :D'
+									: '_message'
+							}`}
+							value={messenger.message}
+							onChange={(e) =>
+								setMessenger({ ...messenger, message: e.target.value })
+							}
+						></textarea>
+						<div className='mb-10'>
+							<Button
+								className='button text-xl mt-10 bg-blue text-black transition-all duration-[0.5s] ease-[ease] px-5 py-2 desktop:px-[30px] desktop:py-3'
+								type='submit'
+							>
+								{state.submitting ? (
+									<BeatLoader style={{ margin: 0 }} size={10} />
+								) : (
+									`{submit}`
+								)}
+							</Button>
+						</div>
+					</form>
+				</motion.section>
+			)}
+		</motion.section>
 	);
 }
--- app/components/ThankYou.tsx
+++ app/components/ThankYou.tsx
@@ -1,15 +1,17 @@
+import { fadeInMotionStyles } from './utils/constants';
 import { BiMailSend } from 'react-icons/bi';
 import React from 'react';
+import { motion } from 'motion/react';
 
 export default function ThankYou() {
 	return (
-		<section className='wrapper h-[70vh]'>
-			<div className='flex flex-col items-center justify-center text-center h-full'>
+		<motion.div {...fadeInMotionStyles}>
+			<div className='flex flex-col items-center justify-center text-center h-full mb-10'>
 				<BiMailSend size={80} color='var(--purple)' />
-				<h1 className='text-[50px] text-yellow dark:text-black'>
+				<h1 className='text-4xl desktop:text-[50px] text-yellow dark:text-black'>
 					Thanks for reaching out! <br /> I&apos;ll get back to you soon.
 				</h1>
 			</div>
-		</section>
+		</motion.div>
 	);
 }
